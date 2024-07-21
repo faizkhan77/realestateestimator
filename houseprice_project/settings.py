ed6cd5fa2029e409396c13b38e442582e2b58570
faiz <randygaming.net@gmail.com>
2024-07-22 03:03:51 +0530

    fix

--- houseprice_project/settings.py
+++ houseprice_project/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ALLOWED_HOSTS = []
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+
 
 # Application definition
 
